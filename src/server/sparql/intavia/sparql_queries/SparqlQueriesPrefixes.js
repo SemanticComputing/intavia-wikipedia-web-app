205299ddd85eee46c79b661f7b85793bc77f35d6
Petri Leskinen <petri.leskinen@aalto.fi>
2023-04-18 23:08:04 +0300

    switch to Intavia/wikipedia model

---
+++ src/server/sparql/intavia/sparql_queries/SparqlQueriesPrefixes.js
@@ -0,0 +1,7 @@
+export const prefixes = `
+  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  prefix wlink: <http://www.intavia.eu/wikipedia/>
+`
