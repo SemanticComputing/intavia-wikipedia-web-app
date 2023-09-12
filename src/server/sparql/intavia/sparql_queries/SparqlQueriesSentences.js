c35f6df2747db9503aad1dd82a05c5573144133d
Petri Leskinen <petri.leskinen@aalto.fi>
2023-09-12 17:43:12 +0300

    Sentences as a facet page

--- src/server/sparql/intavia/sparql_queries/SparqlQueriesSentences.js
+++ src/server/sparql/intavia/sparql_queries/SparqlQueriesSentences.js
@@ -1,8 +1,25 @@
+export const sentencePropertiesFacetResults = `
+  ?id rdfs:label ?prefLabel__id .
+  BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+  BIND(CONCAT("/sentences/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+
+  { 
+    ?id wlink:references ?reference__id .
+    ?reference__id rdfs:label ?reference__prefLabel .
+    BIND(CONCAT("/references/page/", REPLACE(STR(?reference__id), "^.*\\\\/(.+)", "$1")) AS ?reference__dataProviderUrl)
+  }
+  UNION
+  {
+    ?person__id wlink:has_reference ?id ; rdfs:label ?person__prefLabel .
+    BIND(CONCAT("/references/page/", REPLACE(STR(?person__id), "^.*\\\\/(.+)", "$1")) AS ?person__dataProviderUrl)
+  }
+`
+
 export const sentenceProperties = `
     {
       ?id rdfs:label ?prefLabel__id .
       BIND(?prefLabel__id AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/references/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(CONCAT("/sentences/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
       BIND(?id as ?uri__id)
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)
